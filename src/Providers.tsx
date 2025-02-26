08883e026d2225c2b1f191f8705bf3d11c928e70
HodeonArtz <martidicianolold@gmail.com>
2025-02-26 18:58:58 +0100

    feat: add fonts to Mantine theme

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,7 +1,13 @@
 import { createTheme, MantineProvider } from "@mantine/core";
 import { ReactNode } from "react";
 
-const mantineTheme = createTheme({});
+const mantineTheme = createTheme({
+  fontFamily: "Open Sans",
+  headings: {
+    fontFamily: "Epilogue",
+    fontWeight: "600",
+  },
+});
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return <MantineProvider theme={mantineTheme}>{children}</MantineProvider>;

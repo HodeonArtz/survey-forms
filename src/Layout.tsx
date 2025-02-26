c05197b5fb728e6147cf1861c448b74e7741efb9
HodeonArtz <martidicianolold@gmail.com>
2025-02-26 19:41:40 +0100

    refactor: move brand logo to layout folder

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,7 @@
 import { AppShell, Burger, Group } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { Outlet } from "react-router-dom";
-import BrandLogo from "./components/BrandLogo";
+import BrandLogo from "./components/layout/BrandLogo";
 
 const Layout = () => {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
--- src/components/layout/BrandLogo.tsx
+++ src/components/layout/BrandLogo.tsx
@@ -6,8 +6,8 @@ const BrandLogo = () => {
     <Group align="center" gap="6px">
       <IconClipboardSmile stroke={2} color="#7048E8" size="32px" />
       <Box h="28">
-        <Title size="h3" fw="bolder" lts="-2px" c="gray.9">
-          Surveeforms
+        <Title size="h3" fw="bolder" lts="-2px">
+          Surveeform
         </Title>
       </Box>
     </Group>

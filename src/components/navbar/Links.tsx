246d4518f3488bc642ef1224f365eb8b4f7330b9
HodeonArtz <martidicianolold@gmail.com>
2025-02-27 18:51:49 +0100

    feat: set colors to navlinks

--- src/components/navbar/Links.tsx
+++ src/components/navbar/Links.tsx
@@ -1,5 +1,5 @@
-import { NavLink } from "@mantine/core";
-import { Icon, IconProps } from "@tabler/icons-react";
+import { DefaultMantineColor, NavLink } from "@mantine/core";
+import { Icon, IconHome, IconProps } from "@tabler/icons-react";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
@@ -9,26 +9,32 @@ interface NavLinkData {
   label: string;
   href: string;
   description?: string;
+  color?: DefaultMantineColor;
 }
 
 const Links = () => {
   const { t } = useTranslation();
 
-  const navLinks: NavLinkData[] = [{ label: t("routes.home"), href: "/" }];
+  const navLinks: NavLinkData[] = [
+    { label: t("routes.home"), href: "/", icon: IconHome, color: "violet" },
+  ];
   const [active, setActive] = useState(0);
 
-  const links = navLinks.map(({ href, label, description, ...item }, index) => (
-    <Link to={href} style={{ textDecoration: "none" }}>
-      <NavLink
-        key={label}
-        active={index === active}
-        label={label}
-        description={description}
-        leftSection={item.icon && <item.icon size={16} stroke={1.5} />}
-        onClick={() => setActive(index)}
-      />
-    </Link>
-  ));
+  const links = navLinks.map(
+    ({ href, label, description, color, ...item }, index) => (
+      <Link to={href} style={{ textDecoration: "none" }}>
+        <NavLink
+          key={label}
+          active={index === active}
+          label={label}
+          description={description}
+          leftSection={item.icon && <item.icon size={16} stroke={1.5} />}
+          onClick={() => setActive(index)}
+          color={color}
+        />
+      </Link>
+    )
+  );
   return <>{links}</>;
 };
 

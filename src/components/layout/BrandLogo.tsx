7d41d3195e1b5a7d9809f34adce949c30142ce98
HodeonArtz <martidicianolold@gmail.com>
2025-02-28 21:36:17 +0100

    feat: redirect to home when clicking on brand logo

--- src/components/layout/BrandLogo.tsx
+++ src/components/layout/BrandLogo.tsx
@@ -2,20 +2,23 @@ import { Box, Group, Title } from "@mantine/core";
 import { IconClipboardSmile } from "@tabler/icons-react";
 import cx from "clsx";
 import classes from "../../themes.module.css";
+import { Link } from "react-router-dom";
 const BrandLogo = () => {
   return (
-    <Group align="center" gap="6px">
-      <IconClipboardSmile
-        className={cx(classes["brand-logo"])}
-        stroke={2}
-        size="32px"
-      />
-      <Box h="28">
-        <Title size="h3" fw="bolder" lts="-2px">
-          Surveeform
-        </Title>
-      </Box>
-    </Group>
+    <Link to="/" style={{ all: "inherit" }}>
+      <Group align="center" gap="6px" style={{ cursor: "pointer" }}>
+        <IconClipboardSmile
+          className={cx(classes["brand-logo"])}
+          stroke={2}
+          size="32px"
+        />
+        <Box h="28">
+          <Title size="h3" fw="bolder" lts="-2px">
+            Surveeform
+          </Title>
+        </Box>
+      </Group>
+    </Link>
   );
 };
 

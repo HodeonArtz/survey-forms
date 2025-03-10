7c193f1afab290ff966585d63d10dccef07aad38
HodeonArtz <martidicianolold@gmail.com>
2025-03-10 19:25:08 +0100

    feat: create form component for fist form

--- src/pages/surveys/UserDataForm.tsx
+++ src/pages/surveys/UserDataForm.tsx
@@ -1,5 +1,22 @@
+import { InputBase, TextInput } from "@mantine/core";
+import { IMaskInput } from "react-imask";
+
 const UserDataForm = () => {
-  return <>UserDataForm</>;
+  return (
+    <>
+      <TextInput label="What is your name?" placeholder="e.g. John Doe" />
+      <InputBase
+        component={IMaskInput}
+        label="What is your date of birth?"
+        placeholder="DD/MM/YYYY"
+        mask={"00/00/0000"}
+      />
+      <TextInput
+        label="What is your Email?"
+        placeholder="johndoe@example.com"
+      />
+    </>
+  );
 };
 
 export default UserDataForm;

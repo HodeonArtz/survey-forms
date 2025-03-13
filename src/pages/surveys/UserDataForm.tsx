36a76b1133ff65d5e43abb817b595a41f35fc1c8
HodeonArtz <martidicianolold@gmail.com>
2025-03-13 18:38:19 +0100

    feat: build UserDataForm

--- src/pages/surveys/UserDataForm.tsx
+++ src/pages/surveys/UserDataForm.tsx
@@ -1,4 +1,4 @@
-import { InputBase, TextInput } from "@mantine/core";
+import { Autocomplete, InputBase, TagsInput, TextInput } from "@mantine/core";
 import { IMaskInput } from "react-imask";
 
 const UserDataForm = () => {
@@ -11,9 +11,16 @@ const UserDataForm = () => {
         placeholder="DD/MM/YYYY"
         mask={"00/00/0000"}
       />
-      <TextInput
-        label="What is your Email?"
-        placeholder="johndoe@example.com"
+      <TextInput label="What is your Email?" placeholder="johndoe@stucom.com" />
+      <Autocomplete
+        label="What is your genre?"
+        placeholder="Choose one of the options or type other option"
+        data={["Male", "Female", "I prefer not to say"]}
+      />
+      <TagsInput
+        label="What do you do in your free time?"
+        placeholder="Choose the options or write your own option"
+        data={["📕 Read books", "⚽ Sports", "🛫 Travel", "🎬 Watch movies"]}
       />
     </>
   );

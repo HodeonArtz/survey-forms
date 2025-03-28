89bcc1e93dd0059239615f6a8ed5376a83cc94ef
Glen Marti <mdiciano@snapp-solutions.com>
2025-03-28 14:03:53 +0100

    fix: route stuff

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,7 +7,7 @@ import ResultsPage from "./pages/results/ResultsPage";
 // Definimos las rutas asignando a cada una el componente que queremos "renderizar"
 const routes: RouteObject[] = [
   {
-    path: "/",
+    path: "/survey-forms/",
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
